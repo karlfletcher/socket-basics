bdcdf29d4774d95fdd1e1f5f1a60f38dc3acdf7a
Karl Fletcher <karl.fletcher@q2q-it.com>
2017-03-06 16:35:42 +0000

    Added socket message passing for broadcasting messages

--- public/js/app.js
+++ public/js/app.js
@@ -5,5 +5,18 @@ socket.on('connect', function(){
 });
 
 socket.on('message', function(message){
-	console.log(message.text);
+	$('.messages').append("<p> >"+message.text+"</p>")
+});
+
+var $form = $('#message-form');
+
+$form.on('submit', function(e){
+	e.preventDefault();
+
+	var message = $form.find('input[name="message"]').val();
+	socket.emit('message', {
+		text: message
+	});
+
+	$form.find('input[name="message"]').val("").focus();
 });
