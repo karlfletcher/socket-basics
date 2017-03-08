eb81ca23e01258dadf0fd3ebf6cf0091a98e1b98
Karl Fletcher <karl.fletcher@q2q-it.com>
2017-03-08 14:14:44 +0000

    Added currentUsers command

--- server.js
+++ server.js
@@ -13,6 +13,31 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+//Sends user list to provided socket
+function sendCurrentUsers(socket){
+
+	var info = clientInfo[socket.id];
+	var users = [];
+	
+	if(typeof info == 'undefined'){
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function(socketID){
+		var userInfo = clientInfo[socketID];
+		if(userInfo.room == info.room){
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'Server',
+		text: 'Current users: ' + users.join(', '),
+		timestamp: moment().valueOf()
+	})
+
+}
+
 io.on('connection', function(socket){
 	console.log('User connected via socket.io!');
 
@@ -41,7 +66,11 @@ io.on('connection', function(socket){
 	})
 
 	socket.on('message', function(message){
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		
+		if(message.text == "@currentUsers")
+			sendCurrentUsers(socket);
+		else
+			io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
 	socket.emit('message', {
